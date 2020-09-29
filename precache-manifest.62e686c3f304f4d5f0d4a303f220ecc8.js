2ff9437d5f2bd7be8be0ebdb268bf642b0dd22b5
Chris Bubel <csbubel@chriss-mbp.lan>
2020-09-28 21:37:36 -0400

    Updates

---
+++ precache-manifest.62e686c3f304f4d5f0d4a303f220ecc8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f27f2688f17e85886ef2d8d0b68c43f7",
+    "url": "/curated/index.html"
+  },
+  {
+    "revision": "fbf3ce49514030f0c733",
+    "url": "/curated/static/css/main.cd49aa67.chunk.css"
+  },
+  {
+    "revision": "e5deb1388cde9b24793e",
+    "url": "/curated/static/js/2.64fa5d59.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/curated/static/js/2.64fa5d59.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fbf3ce49514030f0c733",
+    "url": "/curated/static/js/main.79ec5491.chunk.js"
+  },
+  {
+    "revision": "9a833e45658473daa510",
+    "url": "/curated/static/js/runtime-main.1c0d3a67.js"
+  }
+]);
