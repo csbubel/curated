2f66e2a22b9a79e243c1a3b6455e4944c3465f90
Chris Bubel <csbubel@chriss-mbp.lan>
2020-09-28 21:19:57 -0400

    Initial build

---
+++ precache-manifest.6cd8fe04c5fbf9428e1bb5efe9aa1189.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e50834bcf8d99a386d8308a7cf0fa64",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3cf02ada0e57cbe351aa",
+    "url": "/static/css/main.3b89da91.chunk.css"
+  },
+  {
+    "revision": "e5deb1388cde9b24793e",
+    "url": "/static/js/2.64fa5d59.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.64fa5d59.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3cf02ada0e57cbe351aa",
+    "url": "/static/js/main.baad5947.chunk.js"
+  },
+  {
+    "revision": "f384574e07d2fedfaa20",
+    "url": "/static/js/runtime-main.fb68eea2.js"
+  }
+]);
